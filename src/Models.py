083790c27a79292dc748d7bccf04ffeeaa3ffe01
THien2304 <hoangthienvtkn@gmail.com>
2026-03-16 18:24:57 +0700

    ffna

--- src/Models.py
+++ src/Models.py
@@ -1,131 +1,60 @@
-import torch
+import pretrainedmodels.models
 import torch.nn as nn
-import torch.nn.functional as F
+import math
+import torch.utils.model_zoo as model_zoo
+import torch
 from torchvision import models
-import timm
+import torch.nn.functional as F
+import pretrainedmodels.models as premodels 
 
-# ---------------- VGG16 ----------------
-class VGG16(nn.Module):
+class Senet101(nn.Module):
     def __init__(self, num_classes=2):
-        super().__init__()
-        model = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)
-        self.features = model.features
-        self.avgpool = model.avgpool
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
-        )
+        super(Senet101, self).__init__()
+        model_se = premodels.se_resnet101()
+        self.layer0 = model_se.layer0
+        self.layer1 = model_se.layer1
+        self.layer2 = model_se.layer2
+        self.layer3 = model_se.layer3
+        self.layer4 = model_se.layer4
+        self.avgpool = model_se.avg_pool
+        self.dropout = model_se.dropout
+        self.fc = nn.Linear(2048, num_classes)
 
     def forward(self, x):
-        x = self.features(x)
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
         x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        return self.classifier(x)
+        if self.dropout is not None:
+            x = self.dropout(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
 
+class Senet50(nn.Module):
+    def __init__(self,num_classes=2):
+        super(Senet50, self).__init__()
+        model_se = premodels.se_resnet50()
+        self.layer0 = model_se.layer0
+        self.layer1 = model_se.layer1
+        self.layer2 = model_se.layer2
+        self.layer3 = model_se.layer3
 
-# ---------------- ResNet ----------------
-class ResNet18(nn.Module):
-    def __init__(self, num_classes=2):
-        super().__init__()
-        m = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
-        self.backbone = nn.Sequential(*list(m.children())[:-1])
-        self.fc = nn.Linear(m.fc.in_features, num_classes)
-
-    def forward(self, x):
-        x = self.backbone(x)
-        return self.fc(torch.flatten(x, 1))
-
-
-class ResNet50(nn.Module):
-    def __init__(self, num_classes=2):
-        super().__init__()
-        m = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
-        self.backbone = nn.Sequential(*list(m.children())[:-1])
-        self.fc = nn.Linear(m.fc.in_features, num_classes)
-
-    def forward(self, x):
-        x = self.backbone(x)
-        return self.fc(torch.flatten(x, 1))
-
-
-class ResNet101(nn.Module):
-    def __init__(self, num_classes=2):
-        super().__init__()
-        m = models.resnet101(weights=models.ResNet101_Weights.IMAGENET1K_V1)
-        self.backbone = nn.Sequential(*list(m.children())[:-1])
-        self.fc = nn.Linear(m.fc.in_features, num_classes)
-
-    def forward(self, x):
-        x = self.backbone(x)
-        return self.fc(torch.flatten(x, 1))
-
-
-# ---------------- DenseNet ----------------
-class DenseNet169(nn.Module):
-    def __init__(self, num_classes=2):
-        super().__init__()
-        m = models.densenet169(weights=models.DenseNet169_Weights.IMAGENET1K_V1)
-        self.features = m.features
-        self.fc = nn.Linear(m.classifier.in_features, num_classes)
-
-    def forward(self, x):
-        x = self.features(x)
-        x = F.relu(x, inplace=True)
-        x = F.adaptive_avg_pool2d(x, 1)
-        return self.fc(torch.flatten(x, 1))
-
-
-class DenseNet201(nn.Module):
-    def __init__(self, num_classes=2):
-        super().__init__()
-        m = models.densenet201(weights=models.DenseNet201_Weights.IMAGENET1K_V1)
-        self.features = m.features
-        self.fc = nn.Linear(m.classifier.in_features, num_classes)
-        self.feature_map = None
-
-    def forward(self, x):
-        x = self.features(x)
-        x = F.relu(x, inplace=True)
-        self.feature_map = x
-        x = F.adaptive_avg_pool2d(x, 1)
-        return self.fc(torch.flatten(x, 1))
-
-    def cam_out(self):
-        return self.feature_map
-
-
-# ---------------- ResNeXt ----------------
-class ResNeXt101(nn.Module):
-    def __init__(self, num_classes=2):
-        super().__init__()
-        m = timm.create_model("resnext101_32x4d", pretrained=True)
-        self.backbone = m.forward_features
-        self.fc = nn.Linear(m.num_features, num_classes)
-
-    def forward(self, x):
-        x = self.backbone(x)
-        return self.fc(x)
-
-
-# ---------------- SENet ----------------
-class SENet50(nn.Module):
-    def __init__(self, num_classes=2, dropout_p=0.2):
-        super().__init__()
-        self.backbone = timm.create_model(
-            "seresnet50",
-            pretrained=True,
-            num_classes=0,
-            global_pool="avg"   
-        )
-        self.dropout = nn.Dropout(dropout_p)
-        self.fc = nn.Linear(self.backbone.num_features, num_classes)
-    def forward(self, x):
-        x = self.backbone(x)     
-        x = self.dropout(x)
+        self.layer4 = model_se.layer4
+        self.avgpool = model_se.avg_pool
+        self.dropout = model_se.dropout
+        self.fc = nn.Linear(2048, num_classes)
+    def forward(self,x):
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        if self.dropout is not None:
+            x = self.dropout(x)
+        x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
