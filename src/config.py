6cdbc026344ebe8dce8339c3b296f2e070e3f6a6
THien2304 <hoangthienvtkn@gmail.com>
2026-03-23 21:03:28 +0700

    new

--- src/config.py
+++ src/config.py
@@ -18,7 +18,7 @@ EARLY_STOPPING_PATIENCE = 15   # số epoch không cải thiện thì dừng
 
 # ==================== HYPERPARAMETERS ====================
 OPTIMIZER = "AdamW"           # "AdamW" hoặc "SGD"
-LR = 1e-4
+LR = 5e-5
 WEIGHT_DECAY = 1e-3
 MOMENTUM = 0.9 if OPTIMIZER == "SGD" else None
 
